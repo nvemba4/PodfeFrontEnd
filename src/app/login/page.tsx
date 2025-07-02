6220a9dfbb51d5d93bbdf835e150102c9994cc3d
nvemba4 <110821108+nvemba4@users.noreply.github.com>
2025-07-02 14:53:29 +0100

    Add files via upload

---
+++ src/app/login/page.tsx
@@ -0,0 +1,8 @@
+export default function Login() {
+    return (
+      <div className="max-w-4xl mx-auto pt-24 p-4">
+        <h1 className="text-3xl font-bold mb-4">doar</h1>
+        <p>Imagens e capturas de tela dos projetos em desenvolvimento.</p>
+      </div>
+    );
+  }
